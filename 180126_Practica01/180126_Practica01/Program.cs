54f56c932c23ce51f3d8ab9a12ff9afcdcfa3b5c
edgarsrodriguez <edgar.rodriguez@cetys.edu.mx>
2018-01-26 19:57:36 -0800

    Add files via upload

---
+++ 180126_Practica01/180126_Practica01/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180126_Practica01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante = new Estudiante("Juan", 18);
+            estudiante.Walk.Caminar();
+            Profesor profesor = new Profesor("Pedro", 32);
+            profesor.Walk.Caminar();
+            Console.ReadLine();
+        }
+    }
+
+    //Objeto
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Walkable Walk { get; set; }
+
+        public void Walking()
+        {
+            Walk.Caminar();
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Comiendo");
+        }
+        public void Poop()
+        {
+            Console.WriteLine("Defecando");
+        }
+    }
+
+    //Herencia de objeto
+    public class Estudiante : Person
+    {
+        public Estudiante(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            Walk = new WalkTwoFeet();
+        }
+    }
+
+    //Herencia de objeto
+    public class Profesor : Person
+    {
+        public Profesor(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            Walk = new WalkFourFeet();
+        }
+    }
+
+    //Implementacion de interfaz
+    public class WalkTwoFeet : Walkable
+    {
+        public void Caminar()
+        {
+            Console.WriteLine("Caminando en 2 pies");
+        }
+    }
+
+    //Implementacion de interfaz
+    public class WalkFourFeet : Walkable
+    {
+        public void Caminar()
+        {
+            Console.WriteLine("Caminando en 4 pies");
+        }
+    }
+
+    //Interfaz
+    public interface Walkable
+    {
+        void Caminar();
+    }
+}
